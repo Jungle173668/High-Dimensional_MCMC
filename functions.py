940e2a46deb711acafa11dd8023de401961e8a72
赵小明 <zhaoxiaoming@Jungles-MacBook-Pro.local>
2025-10-06 19:05:53 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,256 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import distance_matrix
+from scipy.stats import norm
+import matplotlib.cm as cm
+import copy
+
+def Data_generation(D, subsample_factor):
+    Dx = D
+    Dy = D
+    N = Dx * Dy     # Total number of coordinates
+
+    points = [(x, y) for y in np.arange(Dx) for x in np.arange(Dy)]                # Indexes for the inference grid. [0, D]
+    coords = [(x, y) for y in np.linspace(0,1,Dy) for x in np.linspace(0,1,Dx)]    # Coordinates for the inference grid. [0, 1]
+
+    xi, yi = np.array([c[0] for c in points]), np.array([c[1] for c in points])    # Get x, y index lists. [0, D]
+    x, y = np.array([c[0] for c in coords]), np.array([c[1] for c in coords])      # Get x, y coordinate lists. [0, 1]
+
+    ### Data grid defining {vi}i=1,N/subsample_factor - subsampled from inference grid
+    subsample_factor = subsample_factor
+    idx = subsample(N, subsample_factor)
+    M = len(idx)                                                                   # Total number of data points
+
+    ### Generate K, the covariance of the Gaussian process, and sample from N(0,K) using a stable Cholesky decomposition
+    l = 0.2
+    K = GaussianKernel(coords, l)
+    z = np.random.randn(N, )
+    Kc = np.linalg.cholesky(K + 1e-6 * np.eye(N))
+    u = Kc @ z
+
+    ### Observation model: v = G(u) + e,   e~N(0,I)
+    G = get_G(N, idx)
+    v = G @ u + np.random.randn(M)
+
+    return xi, yi, x, y, K, u, idx, N, M, G, v
+
+
+def GaussianKernel(x, l):
+    """ Generate Gaussian kernel matrix efficiently using scipy's distance matrix function"""
+    D = distance_matrix(x, x)
+    return np.exp(-pow(D, 2)/(2*pow(l, 2)))
+
+
+def subsample(N, factor, seed=None):
+    assert factor>=1, 'Subsampling factor must be greater than or equal to one.'
+    N_sub = int(np.ceil(N / factor))
+    if seed: np.random.seed(seed)
+    idx = np.random.choice(N, size=N_sub, replace=False)  # Indexes of the randomly sampled points
+    return idx
+
+
+def get_G(N, idx):
+    """Generate the observation matrix based on datapoint locations.
+    Inputs:
+        N - Length of vector to subsample (length of u)
+        idx - Indexes of subsampled coordinates
+    Outputs:
+        G - Observation matrix"""
+    M = len(idx)
+    G = np.zeros((M, N))
+    for i in range(M):
+        G[i,idx[i]] = 1
+    return G
+
+
+def probit(v):
+    return np.array([0 if x < 0 else 1 for x in v])
+
+
+def predict_t(samples):
+    N = len(samples)
+    M = len(samples[0])
+    MC_sum = np.zeros(M)
+
+    for i in range(N):
+        MC_sum += norm.cdf(samples[i, :])
+
+    return MC_sum / N  # TODO: Return p(t=1|samples)
+
+
+###--- Density functions ---###
+
+def log_prior(u, K, K_inverse):
+    # Return observation likelihood p(u), TODO:
+    N = len(u)
+    constant_term = - 0.5 * (N * np.log(2 * np.pi) + np.linalg.slogdet(K)[1])
+    log_term = - 0.5 * (u.T @ K_inverse @ u)
+    return constant_term + log_term
+
+
+def log_continuous_likelihood(u, v, G):
+    # Return observation likelihood p(v|u), TODO:
+    M = len(v)
+    constant_term = - 0.5 * M * np.log(2*np.pi)
+    distance = np.abs(G @ u - v)
+    exp_term = - 0.5 * (distance.T @ distance)
+    return constant_term + exp_term
+
+def log_probit_likelihood(u, t, G):
+    phi = norm.cdf(G @ u)
+    # TODO: Return likelihood p(t|u)
+    return np.sum([(t[i] == 1) * np.log(phi[i]) + (t[i] == 0) * (np.log(1 - phi[i])) for i in range(G.shape[0])])
+
+
+def log_poisson_likelihood(u, c, G):
+    Gu = np.matmul(G, u)
+    theta = np.exp(Gu)
+    return np.sum(-theta) + np.matmul(c.T,Gu)  # TODO: Return likelihood p(c|u)
+
+
+def log_continuous_target(u, y, K, K_inverse, G):
+    # posterior of u
+    return log_prior(u, K, K_inverse) + log_continuous_likelihood(u, y, G)
+
+
+def log_probit_target(u, t, K_inverse, G):
+    return log_prior(u, K_inverse) + log_probit_likelihood(u, t, G)
+
+
+def log_poisson_target(u, c, K_inverse, G):
+    return log_prior(u, K_inverse) + log_poisson_likelihood(u, c, G)
+
+
+###--- MCMC ---###
+
+def grw(log_target, u0, data, K, G, n_iters, beta):
+    """ Gaussian random walk Metropolis-Hastings MCMC method
+        for sampling from pdf defined by log_target.
+    Inputs:
+        log_target - log-target density
+        u0 - initial sample
+        y - observed data
+        K - prior covariance
+        G - observation matrix
+        n_iters - number of samples
+        beta - step-size parameter
+    Returns:
+        X - samples from target distribution
+        acc/n_iters - the proportion of accepted samples"""
+
+    X = []
+    acc = 0
+    u_prev = u0
+
+    N = len(u0)
+
+    # Inverse computed before the for loop for speed
+    Kc = np.linalg.cholesky(K + 1e-6 * np.eye(N))
+    Kc_inverse = np.linalg.inv(Kc)
+    K_inverse = Kc_inverse.T @ Kc_inverse # TODO: compute the inverse of K using its Cholesky decomopsition
+
+    lt_prev = log_target(u_prev, data, K, K_inverse, G)
+
+    for i in range(n_iters):
+        z = np.random.randn(N)  # Random points
+        u_new = u_prev + beta * Kc @ z  # TODO: Propose new sample - use prior covariance, scaled by beta; Kc@z is under the distribution of N(0,K)
+
+        lt_new = log_target(u_new, data, K, K_inverse, G)
+
+        log_alpha = lt_new - lt_prev # TODO: Calculate acceptance probability based on lt_prev, lt_new
+        log_u = np.log(np.random.random())
+
+        # Accept/Reject
+        accept = log_u <= log_alpha # TODO: Compare log_alpha and log_u to accept/reject sample (accept should be boolean)
+        if accept:
+            acc += 1
+            X.append(u_new)
+            u_prev = u_new
+            lt_prev = lt_new
+        else:
+            X.append(u_prev)
+
+    return X, acc / n_iters
+
+
+def pcn(log_likelihood, u0, y, K, G, n_iters, beta):
+    """ pCN MCMC method for sampling from pdf defined by log_prior and log_likelihood.
+    Inputs:
+        log_likelihood - log-likelihood function
+        u0 - initial sample
+        y - observed data
+        K - prior covariance
+        G - observation matrix
+        n_iters - number of samples
+        beta - step-size parameter
+    Returns:
+        X - samples from target distribution
+        acc/n_iters - the proportion of accepted samples"""
+
+    X = []
+    acc = 0
+    u_prev = u0
+
+    N = len(u0)
+
+    ll_prev = log_likelihood(u_prev, y, G)
+
+    Kc = np.linalg.cholesky(K + 1e-6 * np.eye(N))
+
+    for i in range(n_iters):
+        z = np.random.randn(N)
+        u_new = (np.sqrt(1-beta**2) * u_prev) + (beta * (Kc @ z)) # TODO: Propose new sample using pCN proposal
+
+        ll_new = log_likelihood(u_new, y, G)
+
+        log_alpha = ll_new - ll_prev # TODO: Calculate pCN acceptance probability
+        log_u = np.log(np.random.random())
+
+        # Accept/Reject
+        accept = log_u < log_alpha # TODO: Compare log_alpha and log_u to accept/reject sample (accept should be boolean)
+        if accept:
+            acc += 1
+            X.append(u_new)
+            u_prev = u_new
+            ll_prev = ll_new
+        else:
+            X.append(u_prev)
+
+    return X, acc / n_iters
+
+
+###--- Plotting ---###
+
+def plot_3D(u, x, y, title=None):
+    """Plot the latent variable field u given the list of x,y coordinates"""
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_trisurf(x, y, u, cmap='viridis', linewidth=0, antialiased=False)
+    if title:  plt.title(title)
+    plt.show()
+
+
+def plot_2D(counts, xi, yi, title=None, colors='viridis'):
+    """Visualise count data given the index lists"""
+    Z = -np.ones((max(yi) + 1, max(xi) + 1))
+    for i in range(len(counts)):
+        Z[(yi[i], xi[i])] = counts[i]
+    my_cmap = copy.copy(cm.get_cmap(colors))
+    my_cmap.set_under('k', alpha=0)
+    fig, ax = plt.subplots()
+    im = ax.imshow(Z, origin='lower', cmap=my_cmap, clim=[-0.1, np.max(counts)])
+    fig.colorbar(im)
+    if title:  plt.title(title)
+    plt.show()
+
+
+def plot_result(u, data, x, y, x_d, y_d, title=None):
+    """Plot the latent variable field u with the observations,
+        using the latent variable coordinate lists x,y and the
+        data coordinate lists x_d, y_d"""
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_trisurf(x, y, u, cmap='viridis', linewidth=0, antialiased=False)
+    ax.scatter(x_d, y_d, data, marker='x', color='r')
+    if title:  plt.title(title)
+    plt.show()
